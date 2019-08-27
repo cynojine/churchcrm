d2cc6971d637d1f7e20b7d9d7d84b5526f4ae2e5
Kazashim Kuzasuwat <kazashimkuzasuwat@gmail.com>
2019-08-27 02:11:43 +0200

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class AdminController extends Controller
 {
     //
-	public function login(){
+	public function login(Request $request){
+		if($request->isMethod('post')){
+			$data = $request->input();
+			if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password'],'admin'=>'1'])){
+				echo "success"; die;
+			}else{
+				echo "Faild"; die;
+			}
+		}
 		return view('admin.login');
 		
 	}
