0c23f123ac0330e44b3d0e14b18128c540c30987
Kazashim Kuzasuwat <kazashimkuzasuwat@gmail.com>
2019-09-03 16:08:23 +0200

    update

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -199,15 +199,15 @@
                                     
                                 </ul>
                             </li>
-                            <li class="nav-main-heading"><span class="sidebar-mini-visible">BD</span><span class="sidebar-mini-hidden">Build</span></li>
+                            <li class="nav-main-heading"><span class="sidebar-mini-visible">BD</span><span class="sidebar-mini-hidden">Reports</span></li>
                             <li>
-                                <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-vector"></i><span class="sidebar-mini-hide">Layout</span></a>
+                                <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-vector"></i><span class="sidebar-mini-hide">Admin Reports</span></a>
                                 <ul>
                                     <li>
-                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#">General</a>
+                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#">Members</a>
                                         <ul>
                                             <li>
-                                                <a href="be_layout_default.html">Default</a>
+                                                <a href="be_layout_default.html">Families</a>
                                             </li>
                                             <li>
                                                 <a href="be_layout_flipped.html">Flipped</a>
@@ -217,107 +217,7 @@
                                             </li>
                                         </ul>
                                     </li>
-                                    <li>
-                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#">Header</a>
-                                        <ul>
-                                            <li>
-                                                <a class="nav-submenu" data-toggle="nav-submenu" href="#">Static</a>
-                                                <ul>
-                                                    <li>
-                                                        <a href="be_layout_header_modern.html">Modern</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="be_layout_header_classic.html">Classic</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="be_layout_header_classic_inverse.html">Classic Inverse</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="be_layout_header_glass.html">Glass</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="be_layout_header_glass_inverse.html">Glass Inverse</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a class="nav-submenu" data-toggle="nav-submenu" href="#">Fixed</a>
-                                                <ul>
-                                                    <li>
-                                                        <a href="be_layout_header_fixed_modern.html">Modern</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="be_layout_header_fixed_classic.html">Classic</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="be_layout_header_fixed_classic_inverse.html">Classic Inverse</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="be_layout_header_fixed_glass.html">Glass</a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="be_layout_header_fixed_glass_inverse.html">Glass Inverse</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#">Sidebar</a>
-                                        <ul>
-                                            <li>
-                                                <a href="be_layout_sidebar_inverse.html">Inverse</a>
-                                            </li>
-                                            <li>
-                                                <a href="be_layout_sidebar_hidden.html">Hidden</a>
-                                            </li>
-                                            <li>
-                                                <a href="be_layout_sidebar_mini.html">Mini</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#">Side Overlay</a>
-                                        <ul>
-                                            <li>
-                                                <a href="be_layout_side_overlay_visible.html">Visible</a>
-                                            </li>
-                                            <li>
-                                                <a href="be_layout_side_overlay_hoverable.html">Hoverable</a>
-                                            </li>
-                                            <li>
-                                                <a href="be_layout_side_overlay_no_page_overlay.html">No Page Overlay</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#">Main Content</a>
-                                        <ul>
-                                            <li>
-                                                <a href="be_layout_content_boxed.html">Boxed</a>
-                                            </li>
-                                            <li>
-                                                <a href="be_layout_content_narrow.html">Narrow</a>
-                                            </li>
-                                            <li>
-                                                <a href="be_layout_content_full_width.html">Full Width</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a class="nav-submenu" data-toggle="nav-submenu" href="#">Hero</a>
-                                        <ul>
-                                            <li>
-                                                <a href="be_layout_hero_color.html">Color</a>
-                                            </li>
-                                            <li>
-                                                <a href="be_layout_hero_image.html">Image</a>
-                                            </li>
-                                            <li>
-                                                <a href="be_layout_hero_video.html">Video</a>
-                                            </li>
-                                        </ul>
-                                    </li>
+                                   
                                     <li>
                                         <a href="be_layout_api.html">API</a>
                                     </li>
